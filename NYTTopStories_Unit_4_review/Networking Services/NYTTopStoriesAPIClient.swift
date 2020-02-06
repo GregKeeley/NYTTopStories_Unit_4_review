a0e026766b2beb03fefcc5b80ef14973713f04d3
Gregory Keeley <gregorykeeley@pursuit.org>
2020-02-06 12:52:09 -0500

    - Added APIClient for top stories

---
+++ NYTTopStories_Unit_4_review/Networking Services/NYTTopStoriesAPIClient.swift
@@ -0,0 +1,34 @@
+//
+//  NYTTopStoriesAPIClient.swift
+//  NYTTopStories_Unit_4_review
+//
+//  Created by Gregory Keeley on 2/6/20.
+//  Copyright © 2020 Gregory Keeley. All rights reserved.
+//
+
+import Foundation
+import NetworkHelper
+
+struct NYTTopStoriesAPIClient {
+    static func fetchTopStories(for section: String, completion: @escaping (Result<[Article], AppError>) -> ()) {
+        let endpoint = "https://api.nytimes.com/svc/topstories/v2/nyregion.json?api-key="
+        guard let url = URL(string: endpoint) else {
+            completion(.failure(.badURL(endpoint)))
+            return
+        }
+        let request = URLRequest(url: url)
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let appError):
+                completion(.failure(.networkClientError(appError)))
+            case .success(let articleData):
+                do {
+                    let topStories = try JSONDecoder().decode(TopStories.self, from: articleData)
+                    completion(.success(topStories.results))
+                } catch {
+                    completion(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
+}
