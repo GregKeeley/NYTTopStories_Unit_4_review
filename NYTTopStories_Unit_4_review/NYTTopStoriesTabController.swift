d9b2dfe27a85f7ff178f007a0ec8b3f72d2f4ef6
Gregory Keeley <gregorykeeley@pursuit.org>
2020-02-06 10:50:47 -0500

    - Created TabBarController for rootViewController - Set TabBarViewController as root

---
+++ NYTTopStories_Unit_4_review/NYTTopStoriesTabController.swift
@@ -0,0 +1,21 @@
+//
+//  NYTTopStoriesTabController.swift
+//  NYTTopStories_Unit_4_review
+//
+//  Created by Gregory Keeley on 2/6/20.
+//  Copyright © 2020 Gregory Keeley. All rights reserved.
+//
+
+import UIKit
+
+class NYTTopStoriesTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+    
+
+   
+
+}
